c1fc5c26351d591955a297dc17d65e97fc2fa3f6
Alex Garcia <hi.im.alex.j.garcia@gmail.com>
2020-05-24 21:00:33 -0700

    Modified App.tsx, creating generic panes. Using simple editor.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
-function App() {
+import Card from 'react-bootstrap/Card'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
+import Collapse from 'react-bootstrap/Collapse'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import Editor from 'react-simple-code-editor'
+
+import useLocalStorageState from './hooks/useLocalStorageState'
+
+function App () {
+  const [open, setOpen] = useState(false)
+  const [code, setCode] = useLocalStorageState('', 'semscreen')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Row>
+        <Collapse in={open} timeout={0}>
+          <Col sm='3'>
+            <div id='collapse-panel' className='SemscreensPane'>
+              <Card>
+                <Card.Body>
+                  <p>List of Semscreens</p>
+                </Card.Body>
+              </Card>
+            </div>
+          </Col>
+        </Collapse>
+        <Col>
+          <div className='SemscreenPane'>
+            <Card>
+              <Card.Body>
+                <Editor
+                  highlight={code => code}
+                  value={code}
+                  onValueChange={code => setCode(code)}
+                  padding={10}
+                  style={{
+                    fontFamily: '"Fira code", "Fira Mono", monospace',
+                    fontSize: 12
+                  }}
+                />
+                <Button
+                  size='sm'
+                  onClick={() => setOpen(!open)}
+                  aria-controls='collapse-panel'
+                  aria-expanded={open}
+                >
+                    Click
+                </Button>
+              </Card.Body>
+            </Card>
+          </div>
+        </Col>
+      </Row>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/hooks/useLocalStorageState.tsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react';
+
+export default function useLocalStorageState(defaultValue: string, key: string) {
+  const [value, setValue] = useState(() => {
+    const localStorageValue = window.localStorage.getItem(key);
+    return localStorageValue !== null
+      ? JSON.parse(localStorageValue)
+      : defaultValue;
+  });
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+  return [value, setValue];
+}

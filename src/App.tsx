b8e78f8e8cbb47b7c651982ff13ce8739e0c1e52
Alex Garcia <hi.im.alex.j.garcia@gmail.com>
2020-05-25 00:46:36 -0700

    Removed Bootstrap grid.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import React, { useState } from 'react'
 import './App.css'
 
-import Card from 'react-bootstrap/Card'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
-import Collapse from 'react-bootstrap/Collapse'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import Editor from 'react-simple-code-editor'
@@ -18,45 +14,26 @@ function App () {
 
   return (
     <div className='App'>
-      <Row>
-        <Collapse in={open} timeout={0}>
-          <Col sm='3'>
-            <div id='collapse-panel' className='SemscreensPane'>
-              <Card>
-                <Card.Body>
-                  <p>List of Semscreens</p>
-                </Card.Body>
-              </Card>
-            </div>
-          </Col>
-        </Collapse>
-        <Col>
-          <div className='SemscreenPane'>
-            <Card>
-              <Card.Body>
-                <Editor
-                  highlight={code => code}
-                  value={code}
-                  onValueChange={code => setCode(code)}
-                  padding={10}
-                  style={{
-                    fontFamily: '"Fira code", "Fira Mono", monospace',
-                    fontSize: 12
-                  }}
-                />
-                <Button
-                  size='sm'
-                  onClick={() => setOpen(!open)}
-                  aria-controls='collapse-panel'
-                  aria-expanded={open}
-                >
+      <div className='SemscreenPane'>
+        <Editor
+          highlight={code => code}
+          value={code}
+          onValueChange={code => setCode(code)}
+          padding={10}
+          style={{
+            fontFamily: '"Fira code", "Fira Mono", monospace',
+            fontSize: 12
+          }}
+        />
+        <Button
+          size='sm'
+          onClick={() => setOpen(!open)}
+          aria-controls='collapse-panel'
+          aria-expanded={open}
+        >
                     Click
-                </Button>
-              </Card.Body>
-            </Card>
-          </div>
-        </Col>
-      </Row>
+        </Button>
+      </div>
     </div>
   )
 }

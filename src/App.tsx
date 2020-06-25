9b247c674c63f3d82af81d34f14cd6b8a96715ef
Alex Garcia <hi.im.alex.j.garcia@gmail.com>
2020-06-25 14:56:49 -0700

    Updated packages. Added Bootstrap Icons. Aligned slide-out panel button to left side of screen.

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,15 @@ import Button from 'react-bootstrap/Button'
 import Modal from 'react-bootstrap/Modal'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import chevronRight from 'bootstrap-icons/icons/chevron-right.svg'
+
 import Editor from 'react-simple-code-editor'
 
 import useLocalStorageState from './hooks/useLocalStorageState'
 
 function App () {
   const [showPanel, setShowPanel] = useState(false)
-  const [semscreen, setSemscreen] = useLocalStorageState('', 'semscreen')
+  const [semscreen, setSemscreen] = useLocalStorageState('{}', 'semscreen')
 
   const handleShowPanel = () => setShowPanel(true)
   const handleClosePanel = () => setShowPanel(false)
@@ -19,7 +21,7 @@ function App () {
   return (
     <div className='App'>
 
-      <div className='SemscreenPane'>
+      <div className='SemscreenPane position-relative'>
         <Editor
           highlight={semscreen => semscreen}
           value={semscreen}
@@ -27,18 +29,31 @@ function App () {
           padding={10}
           style={{
             fontFamily: '"Fira code", "Fira Mono", monospace',
-            fontSize: 12
+            fontSize: 12,
+            marginLeft: '2rem'
           }}
         />
-        <Button onClick={handleShowPanel}>Click</Button>
+        <Button
+	  variant='light'
+	  size='sm'
+	  className='position-absolute h-100'
+          style={{ top: 0, left: 0 }}
+	  onClick={handleShowPanel}
+	>
+          <img src={chevronRight} alt="Chevron right" />
+	</Button>
       </div>
 
       <div className='SemscreenListPane'>
         <Modal size='sm' show={showPanel} onHide={handleClosePanel} dialogClassName='modal-dialog-slideout'>
           <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
+            <Modal.Title>Screens</Modal.Title>
           </Modal.Header>
-          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Body>
+	  <ul>
+	    <li>Mine</li>
+	  </ul>
+	  </Modal.Body>
           <Modal.Footer>
             <Button variant='secondary' onClick={handleClosePanel}>
               Close

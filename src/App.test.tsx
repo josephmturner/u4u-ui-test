fcc35b5930cf2e48ee254be0360dbd1845bb3ef4
Alex Garcia <hi.im.alex.j.garcia@gmail.com>
2020-07-07 02:09:18 -0700

    Upgraded packages, ran prettier.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-import App from './App'
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders panel button', () => {
-  const { getByRole } = render(<App />)
-  const buttonElement = getByRole('button', {name: /Activate panel/i})
-  expect(buttonElement).toBeInTheDocument()
-})
+test("renders panel button", () => {
+  const { getByRole } = render(<App />);
+  const buttonElement = getByRole("button", { name: /Activate panel/i });
+  expect(buttonElement).toBeInTheDocument();
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,77 @@
-import React, { useState } from 'react'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
 
-import Button from 'react-bootstrap/Button'
-import Modal from 'react-bootstrap/Modal'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import chevronRight from 'bootstrap-icons/icons/chevron-right.svg'
+import chevronRight from "bootstrap-icons/icons/chevron-right.svg";
 
-import Editor from 'react-simple-code-editor'
+import Editor from "react-simple-code-editor";
 
-import useLocalStorageState from './hooks/useLocalStorageState'
+import useLocalStorageState from "./hooks/useLocalStorageState";
 
-function App () {
-  const [showPanel, setShowPanel] = useState(false)
-  const [semscreen, setSemscreen] = useLocalStorageState('{}', 'semscreen')
+function App() {
+  const [showPanel, setShowPanel] = useState(false);
+  const [semscreen, setSemscreen] = useLocalStorageState("{}", "semscreen");
 
-  const handleShowPanel = () => setShowPanel(true)
-  const handleClosePanel = () => setShowPanel(false)
+  const handleShowPanel = () => setShowPanel(true);
+  const handleClosePanel = () => setShowPanel(false);
 
   return (
-    <div className='App'>
-
-      <div className='SemscreenPane position-relative'>
+    <div className="App">
+      <div className="SemscreenPane position-relative">
         <Editor
-          highlight={semscreen => semscreen}
+          highlight={(semscreen) => semscreen}
           value={semscreen}
-          onValueChange={semscreen => setSemscreen(semscreen)}
+          onValueChange={(semscreen) => setSemscreen(semscreen)}
           padding={10}
           style={{
             fontFamily: '"Fira code", "Fira Mono", monospace',
             fontSize: 12,
-            marginLeft: '2rem'
+            marginLeft: "2rem",
           }}
         />
         <Button
-	  variant='light'
-	  size='sm'
-	  className='position-absolute h-100'
+          variant="light"
+          size="sm"
+          className="position-absolute h-100"
           style={{ top: 0, left: 0 }}
-	  onClick={handleShowPanel}
-	  role='button'
-	  aria-label='Activate panel'
-	>
+          onClick={handleShowPanel}
+          role="button"
+          aria-label="Activate panel"
+        >
           <img src={chevronRight} alt="Chevron right" />
-	</Button>
+        </Button>
       </div>
 
-      <div className='SemscreenListPane'>
-        <Modal size='sm' show={showPanel} onHide={handleClosePanel} dialogClassName='modal-dialog-slideout'>
+      <div className="SemscreenListPane">
+        <Modal
+          size="sm"
+          show={showPanel}
+          onHide={handleClosePanel}
+          dialogClassName="modal-dialog-slideout"
+        >
           <Modal.Header closeButton>
             <Modal.Title>Screens</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-	  <ul>
-	    <li>Mine</li>
-	  </ul>
-	  </Modal.Body>
+            <ul>
+              <li>Mine</li>
+            </ul>
+          </Modal.Body>
           <Modal.Footer>
-            <Button variant='secondary' onClick={handleClosePanel}>
+            <Button variant="secondary" onClick={handleClosePanel}>
               Close
             </Button>
-            <Button variant='primary' onClick={handleClosePanel}>
+            <Button variant="primary" onClick={handleClosePanel}>
               Save Changes
             </Button>
           </Modal.Footer>
         </Modal>
       </div>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
